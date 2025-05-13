80e54223643c004fd3f15730f5aeb7f583a1f614
Nikola Valkov <bulgarianmapper64@gmail.com>
2025-05-13 18:34:23 +0300

    Added all necessery tables for assigment requirments

---
+++ api/src/main/java/booky/nikolabv/model/OrderItem.java
@@ -0,0 +1,38 @@
+package booky.nikolabv.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "OrderItems")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private PurchaseOrder order;
+    
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    
+    private Integer quantity;
+    
+    private Double priceAtPurchase;
+}
---
+++ api/src/main/java/booky/nikolabv/model/Product.java
@@ -0,0 +1,45 @@
+package booky.nikolabv.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+    
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    
+    @Column(nullable = false)
+    private Double price;
+    
+    private Integer stockQuantity;
+    
+    @Column(length = 50)
+    private String sku;
+    
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
